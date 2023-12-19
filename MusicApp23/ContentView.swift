b0830368bccc3804f94c09dd9c95c85c0db44dfb
Dima-Bulgakov <bulgakovdu@gmail.com>
2023-12-19 18:48:10 +0200

    Initial Commit

---
+++ MusicApp23/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicApp23
+//
+//  Created by Dima on 19.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicApp23/MusicApp23App.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp23App.swift
+//  MusicApp23
+//
+//  Created by Dima on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
